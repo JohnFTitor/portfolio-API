d24a4e76d1488ef1cfea20b944071d9646b9feee
Andres Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2025-02-23 12:44:10 -0500

    Add tests

---
+++ portfolio/user/tests/factory.py
@@ -0,0 +1,23 @@
+from portfolio.user import models
+
+
+def user(
+    username='test',
+    email='test@email.com',
+    password='password',
+    **kwargs,
+):
+    save = kwargs.pop('save', False)
+
+    user = models.User(
+        username=username,
+        email=email,
+        **kwargs,
+    )
+
+    user.set_password(password)
+
+    if save:
+        user.save()
+
+    return user
--- portfolio/user/tests/test_login.py
+++ portfolio/user/tests/test_login.py
@@ -1,9 +1,60 @@
 from django.test import TestCase
 from django.urls import reverse as url_reverse
+from rest_framework import status
+import factory
 
 class TestLoginView(TestCase):
   def setUp(self):
     self.url = url_reverse('user_login')
 
   def test_400_error_wrong_credentials(self):
-    pass
+    user = factory.user(
+      password='test',
+      save=True,
+    )
+
+    response = self.client.post(
+      self.url,
+      data={
+        'username': user.username,
+        'password': 'wrong pass',
+      },
+    )
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+  def test_400_error_wrong_user(self):
+    factory.user(
+      password='test',
+      save=True,
+    )
+
+    response = self.client.post(
+      self.url,
+      data={
+        'username': 'random',
+        'password': 'wrong pass',
+      },
+    )
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+  def test_200_with_good_credentials(self):
+    user = factory.user(
+      password='Test',
+      save=True,
+    )
+
+    response = self.client.post(
+      self.url,
+      data={
+        'username': user.username,
+        'password': 'Test',
+      }
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    assert 'token' in response.data
+
+
