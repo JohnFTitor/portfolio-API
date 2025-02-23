523fbd69f146ec741b4c33c054c9161a708be400
Andres Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2025-02-23 12:47:41 -0500

    Format

--- portfolio/user/tests/test_login.py
+++ portfolio/user/tests/test_login.py
@@ -4,62 +4,61 @@ from rest_framework import status
 import factory
 from knox.models import AuthToken
 
+
 class TestLoginView(TestCase):
-  def setUp(self):
-    self.url = url_reverse('user_login')
+    def setUp(self):
+        self.url = url_reverse('user_login')
 
-  def test_400_error_wrong_credentials(self):
-    user = factory.user(
-      password='test',
-      save=True,
-    )
+    def test_400_error_wrong_credentials(self):
+        user = factory.user(
+            password='test',
+            save=True,
+        )
 
-    response = self.client.post(
-      self.url,
-      data={
-        'username': user.username,
-        'password': 'wrong pass',
-      },
-    )
+        response = self.client.post(
+            self.url,
+            data={
+                'username': user.username,
+                'password': 'wrong pass',
+            },
+        )
 
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
 
-  def test_400_error_wrong_user(self):
-    factory.user(
-      password='test',
-      save=True,
-    )
+    def test_400_error_wrong_user(self):
+        factory.user(
+            password='test',
+            save=True,
+        )
 
-    response = self.client.post(
-      self.url,
-      data={
-        'username': 'random',
-        'password': 'wrong pass',
-      },
-    )
+        response = self.client.post(
+            self.url,
+            data={
+                'username': 'random',
+                'password': 'wrong pass',
+            },
+        )
 
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
 
-  def test_200_with_good_credentials(self):
-    user = factory.user(
-      password='Test',
-      save=True,
-    )
+    def test_200_with_good_credentials(self):
+        user = factory.user(
+            password='Test',
+            save=True,
+        )
 
-    response = self.client.post(
-      self.url,
-      data={
-        'username': user.username,
-        'password': 'Test',
-      }
-    )
+        response = self.client.post(
+            self.url,
+            data={
+                'username': user.username,
+                'password': 'Test',
+            }
+        )
 
-    assert response.status_code == status.HTTP_200_OK
+        assert response.status_code == status.HTTP_200_OK
 
-    assert 'token' in response.data
+        assert 'token' in response.data
 
-    assert AuthToken.objects.filter(
-      user=user,
-    ).exists()
-
-
+        assert AuthToken.objects.filter(
+            user=user,
+        ).exists()
